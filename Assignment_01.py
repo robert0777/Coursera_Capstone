1d985942400e2aa08a1408b4caad589a7ec8f1d5
Robert Hernández <robert@actuariayfinanzas.net>
2020-04-19 17:46:25 -0500

    First assignment Module 09

--- Assignment_01.py
+++ Assignment_01.py
@@ -2,17 +2,24 @@
 # coding: utf-8
 
 # This notebook will be used for the Capstone Project in Module 09 of the IBM Certification  
+#   
+#   
 # Dr. Robert Hernández  
-# robert@actuariayfinanzas.net
+#   
+#   
+# robert@actuariayfinanzas.net  
+#   
+#     
+#   https://www.youracclaim.com/users/robert-hernandez.89bffe7b
 
-# In[1]:
+# In[3]:
 
 
 import pandas as pd
 import numpy as np
 
 
-# In[2]:
+# In[4]:
 
 
 print("Hello Capstone Project Course!")
